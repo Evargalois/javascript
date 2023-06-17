205a3975ba4dd99d9b1980407f54090bc95d860a
Evargalois <104255605+Evargalois@users.noreply.github.com>
2023-06-17 20:04:00 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,23 +15,10 @@ document.getElementById('translationForm').addEventListener('submit', function(e
  });
  
 // Fonction pour ajouter le mot et sa traduction dans le stockage local
-// Fonction pour ajouter le mot et sa traduction dans Firebase
 function addTranslation(word, translation) {
-  // Ajoutez les données à la collection 'translations'
-  db.collection('translations').add({
-    word: word,
-    translation: translation
-  })
-  .then(function(docRef) {
-    console.log('Mot ajouté avec succès. ID du document :', docRef.id);
-    // Mettre à jour l'affichage ou effectuer d'autres actions si nécessaire
-  })
-  .catch(function(error) {
-    console.error('Une erreur est survenue lors de l\'ajout du mot :', error);
-    // Gérer l'erreur de manière appropriée
-  });
-}
-
+  // Vérifier si des traductions existent déjà dans le stockage local
+  var translations = localStorage.getItem('translations');
+  var translationsObj = translations ? JSON.parse(translations) : {};
 
   // Ajouter le nouveau mot et sa traduction
   translationsObj[word] = translation;
